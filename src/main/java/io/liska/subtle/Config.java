5d46690ed0cef7c39717ed3f4531e815496ca77a
Raxmo <maxsroe@gmail.com>
2025-09-14 07:54:03 -0700

    DONE: - project clean-up > [config class]   - config template ACTIVE: TODO: - single structures   - Signs   - houses - inventory fuckery   - shuffle - base screwery   - random blocks   - random signs   - mirrored - world-gen stuffs   - change seed periodicly   - lift generated chunks   - cross-world chunks

--- src/main/java/io/liska/subtle/Config.java
+++ src/main/java/io/liska/subtle/Config.java
@@ -8,28 +8,36 @@ import net.neoforged.neoforge.common.ModConfigSpec;
 
 // An example config class. This is not required, but it's a good idea to have one to keep your config organized.
 // Demonstrates how to use Neo's config APIs
+
+// TODO: populate with actually useful stuffs
+
 public class Config {
     private static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
+    private static final ModConfigSpec SPEC;
 
-    public static final ModConfigSpec.BooleanValue LOG_DIRT_BLOCK = BUILDER
-            .comment("Whether to log the dirt block on common setup")
-            .define("logDirtBlock", true);
+    // ------------ TEMPLATE ------------ //
+    public static final ModConfigSpec.IntValue TEMPLATE_NUMBER;
+    public static final ModConfigSpec.BooleanValue TEMPLATE_BOOL;
+    public static final ModConfigSpec.DoubleValue TEMPLATE_DOUBLE;
+    static
+    {
+        BUILDER.push("Template");
 
-    public static final ModConfigSpec.IntValue MAGIC_NUMBER = BUILDER
-            .comment("A magic number")
-            .defineInRange("magicNumber", 42, 0, Integer.MAX_VALUE);
+        TEMPLATE_NUMBER = BUILDER
+                .comment("A template int")
+                .defineInRange("Template Number", 42, 0, 255);
 
-    public static final ModConfigSpec.ConfigValue<String> MAGIC_NUMBER_INTRODUCTION = BUILDER
-            .comment("What you want the introduction message to be for the magic number")
-            .define("magicNumberIntroduction", "The magic number is... ");
+        TEMPLATE_BOOL = BUILDER
+                .comment("A template boolean")
+                .define("TemplateBool", false);
 
-    // a list of strings that are treated as resource locations for items
-    public static final ModConfigSpec.ConfigValue<List<? extends String>> ITEM_STRINGS = BUILDER
-            .comment("A list of items to log on common setup.")
-            .defineListAllowEmpty("items", List.of("minecraft:iron_ingot"), () -> "", Config::validateItemName);
+        TEMPLATE_DOUBLE = BUILDER
+                .comment("A template double")
+                .defineInRange("Template Double", 0.5, 0.0, 1.0);
 
-    static final ModConfigSpec SPEC = BUILDER.build();
+    }
 
+    static{ SPEC = BUILDER.build(); }
     private static boolean validateItemName(final Object obj) {
         return obj instanceof String itemName && BuiltInRegistries.ITEM.containsKey(ResourceLocation.parse(itemName));
     }
